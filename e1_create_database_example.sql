7bf171e668057f86e241dc42d0f5d1653eb3c707
Tiger Johnson <69415529+lance7777@users.noreply.github.com>
2024-02-05 14:34:39 -0500

    commands to create a database called "example"

---
+++ e1_create_database_example.sql
@@ -0,0 +1,80 @@
+-- \c postgres <<superuser_server_admin>> 
+--
+
+CREATE ROLE example_dbo; 
+
+GRANT example_dbo TO <<superuser_server_admin>>; 
+
+CREATE DATABASE example OWNER example_dbo; 
+REVOKE ALL ON DATABASE example FROM PUBLIC; 
+
+GRANT CREATE ON DATABASE example TO example_dbo; 
+
+
+CREATE ROLE example_basic_reader WITH LOGIN ENCRYPTED PASSWORD 'fake_password_1'; 
+CREATE ROLE example_data_staff WITH LOGIN ENCRYPTED PASSWORD 'fake_password_2'; 
+CREATE ROLE example_backend_service WITH LOGIN ENCRYPTED PASSWORD 'fake_password_3'; 
+
+
+GRANT CONNECT ON DATABASE example TO example_basic_reader; 
+GRANT CONNECT ON DATABASE example TO example_data_staff; 
+GRANT CONNECT ON DATABASE example TO example_backend_service; 
+
+
+CREATE ROLE example_reader_reference; 
+
+GRANT example_reader_reference TO example_basic_reader; 
+GRANT example_reader_reference TO example_data_staff; 
+
+
+CREATE ROLE example_writer_reference; 
+
+GRANT example_writer_reference TO example_data_staff; 
+
+
+CREATE ROLE example_exec_reference; 
+
+GRANT example_exec_reference TO example_backend_service; 
+
+
+\c example 
+
+DROP SCHEMA IF EXISTS "public" RESTRICT
+--
+; 
+
+-- # # # # # # # # # # 
+SET ROLE example_dbo; 
+-- # # # # # # # # # # 
+
+
+ALTER DATABASE example SET search_path = reference, "utility"; 
+
+
+-- CREATE SCHEMA IF NOT EXISTS utility;
+
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA utility; 
+-- CREATE EXTENSION IF NOT EXISTS "btree_gist" SCHEMA utility; 
+
+
+--
+--
+
+/*
+
+SELECT X.oid 
+ , X.datname 
+ , X.datdba 
+ , pg_catalog.pg_get_userbyid(X.datdba) AS datdba_name 
+FROM pg_catalog.pg_database AS X 
+ORDER BY X.oid ASC 
+--
+; 
+
+SELECT * 
+FROM information_schema.schemata; 
+
+*/
+
+--
+--
